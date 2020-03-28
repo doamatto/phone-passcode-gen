2accdfe58901ac634e253670973c075177aa5668
doamatto <hello@doamatto.xyz>
2020-03-28 09:28:30 -0700

    A lot of rewriting for way better working stuff

--- gen.js
+++ gen.js
@@ -6,9 +6,32 @@
 */
 
 function gen() {
-  var elm = document.getElementById("passcode");
-  var pin = Math.floor(Math.random() * Math.floor(max)); // This makes a random number that gets the loop number appended
-                                                        // to the variable. I need to find an eval() alternative
-  elm.innerHTML = `Your passcode is: ${pin}`;
-  console.log("We have lift off.");
+  var c = document.getElementById("pincount").value; // Take the number of numbers you want
+  if(!c) {
+    c = 6;
+  }
+  var i; // Used for variable looping
+  var num = new Array(); // Defines array for passcode
+  var elm = document.getElementById("passcode"); // For easier calling of the passcode element later
+
+  for (i=0; i<c; i++) {
+    num[i] = Math.floor(Math.random() * Math.floor(9)); // Generate one number
+  }
+  
+  var pin = num.toString(); // Turns array into string
+  var pin1 = pin.replace(/,/g,""); // Removes commas in array string
+
+  document.getElementById("gen").style.display = "block"; // Makes passcode visible;
+  noError(); // Reports no issue occuring
+  elm.innerHTML = `Your passcode is: ${pin1}`; // Brings PIN into document
+  console.log("We have lift off."); // Let's you know everything went a-okay.
+  pin = null; // Resets PIN variable to reduce some peeking
 }
+
+function noError() {
+  // If an error doesn't happen, this function simply hides the error elements.
+  var elm = document.getElementById("error");
+  elm.style.display = "none";
+  return;
+}
+
