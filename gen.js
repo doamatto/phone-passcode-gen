5ca8f47e8ba01e80dc0716ba18b8a38e8521da29
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-21 00:55:20 -0700

    Minimizations and cleaning

--- gen.js
+++ gen.js
@@ -1,23 +1,18 @@
 function gen() {
+  var pCount = document.getElementById("pincount").value;
   var n;
   try { // Check if you can turn the pincount into a number. If not, fail.
-    n = Number(document.getElementById("pincount").value);
-  } catch (err) {
-    return console.error("The number of numbers provided could not be converted into an integer.");
-  }
-  if (isNaN(n)) {
-    return console.error("The number of numbers provided is not a real integer.");
-  }
-  if (n <= 0) { // Check if provided number is less than 0. If not, succeed.
-    return console.error("The number of numbers prvoided is equal to or less than 0.");
-  }
-  if(!document.getElementById("pincount").value) { // Check if you've provided the amount of digits for your PIN
+    n = Number(pCount);
+  } catch (err)
+  return console.error("The number of numbers provided could not be converted into an integer.");
+  if (isNaN(n)) return console.error("The number of numbers provided is not a real integer.");
+  if (n <= 0) return console.error("The number of numbers prvoided is equal to or less than 0."); // Check if provided number is less than 0. If not, succeed.
+  if(!pCount) { // Check if you've provided the amount of digits for your PIN
     c = 6; // Default to 6 digits
   } else {
-    c = document.getElementById("pincount").value; // Take the number of numbers you want
+    c = pCount; // Take the number of numbers you want
   }
-  var i; // Used for variable looping
-  var num = []; // Defines array for passcode
+  var i, num; // Used for variable looping and defines array for passcode
   var elm = document.getElementById("passcode"); // For easier calling of the passcode element later
 
   for (i=0; i<c; i++) {
@@ -25,30 +20,27 @@ function gen() {
   }
   
   var pin = num.toString(); // Turns array into string
-  var pin1 = pin.replace(/,/g,""); // Removes commas in array string
+  pin = pin.replace(/,/g,""); // Removes commas in array string
 
   document.getElementById("gen").style.display = "block"; // Makes passcode visible;
   noError(); // Reports no issue occuring
-  elm.innerHTML = `Your passcode is: ${pin1}`; // Brings PIN into document
-  console.log("We have lift off."); // Let's you know everything went a-okay.
+  elm.innerHTML = `Your passcode is: ${pin}`; // Brings PIN into document
   pin = null; // Resets PIN variable to reduce some peeking
 }
 
 function noError() {
-  // If an error doesn't happen, this function simply hides the error elements.
   var elm = document.getElementById("error");
-  elm.style.display = "none";
-  return;
-}
+  return elm.style.display = "none";
+}); // If an error doesn't happen, this function simply hides the error elements.
 
-function handlingError(msg, url, line) {
+function handlingError(msg, url, line => {
   console.error("Error logged locally for when announced to user.");
   var elm = document.getElementById("errorText");
   elm.innerHTML = (`An error has occured on line ${line} of ${url}. The following is what was captured: ${msg}`);
   document.getElementById("error").style.display = "block";
   document.genElementById("gen").style.display = "none";
-}
+});
 
-window.onerror = function (msg, url, line) {
+window.onerror = (msg, url, line => {
   handlingError(msg, url, line);
-};
+});
