e0df36153ff24197fb57bc870e2a7c0f47743064
Matt Ronchetto <hello@doamatto.xyz>
2021-06-18 16:20:12 -0700

    chore: added typings et removed defunct

--- gen.ts
+++ gen.ts
@@ -1,21 +1,13 @@
 function gen () {
-  const pCount = document.getElementById('pincount').value
-  let c
+  const pCount = (<HTMLInputElement>document.getElementById('pincount')).value
+  let c: number
   let num = []
 
   // Check if value isn't blank
   if (pCount === '' || pCount === undefined || pCount === null) {
     c = 6 // Default to a six-digit pin
   } else {
-    c = pCount // Pass-forward
-  }
-
-  // Check if value is a number
-  try {
-    const n = Number(c)
-    if (isNaN(n)) { return console.error('Placeholder to throw error') }
-  } catch (e) {
-    return console.error('The number of numbers provided couldn\'t be converted to or isn\'t a real number')
+    c = parseFloat(pCount) // Pass-forward
   }
 
   // Check if value is a usable number (negative numbers)
@@ -25,12 +17,12 @@ function gen () {
   c = Math.round(c)
 
   // Generate numbers and ensures each is a whole number
-  for (let i = 0; i < c; i++) {
+  for (let i:number = 0; i < c; i++) {
     num[i] = Math.floor(Math.random() * Math.floor(9))
   }
 
   // Converts numbers generated into a usable string
-  let pin = num.toString().replace(/,/g, '') // Removes commas from old array
+  let pin:string = num.toString().replace(/,/g, '') // Removes commas from old array
 
   // Put passcode into document
   document.getElementById('gen').style.display = 'block'
@@ -42,7 +34,7 @@ function gen () {
   pin = null
 }
 
-function handlingError (msg, url, line) {
+function handlingError (msg: string | Event, url: string, line: number) {
   console.error('Error logged locally for when announced to user.')
   const elm = document.getElementById('error')
   elm.innerHTML = (`An error has occured on line ${line} of ${url}. The following is what was captured: ${msg}`)
@@ -54,9 +46,8 @@ window.onerror = (msg, url, line) => {
   handlingError(msg, url, line)
 }
 
-window.addEventListener('DOMContentLoaded', (e) => {
-  document.getElementsByClassName('applet')[0].style.display = 'block'
-  document.getElementById('genBTN').addEventListener('click', (e) => {
-    gen()
-  })
+window.addEventListener('DOMContentLoaded', () => {
+  var applet = Array.from(document.getElementsByClassName('applet') as HTMLCollectionOf<HTMLElement>)
+  applet[0].style.display = 'block'
+  document.getElementById('genBTN').addEventListener('click', () => { gen() })
 })
